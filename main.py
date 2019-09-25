a288552f3a22b29f85662c5cb006ca5042d07941
Leeeon233 <leeeon233@gmail.com>
2019-09-25 21:01:20 +0800

    test docker

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import json
 
 root = '/tcdata/guangdong1_round2_testA_20190924'
 result = []
-for file in os.listdir(root):
-    result.append(
-        {'name': file, 'category': 1, 'bbox': [100, 200, 100, 200], 'score': 1.0})
+for dir_name in os.listdir(root):
+    for file in os.listdir(os.path.join(root, dir_name)):
+        if not file.startswith('template'):
+            result.append(
+                {'name': file, 'category': 1, 'bbox': [100, 200, 100, 200], 'score': 1.0})
 
 with open('result.json', 'w') as fp:
     json.dump(result, fp, indent=4, separators=(',', ': '))
