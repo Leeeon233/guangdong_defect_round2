7d8755b8ca581bb2ff4c6cbf2159951133c59148
Leeeon233 <leeeon233@gmail.com>
2019-10-05 10:46:58 +0800

    siamese hrnet

--- mmdetection/mmdet/models/backbones/__init__.py
+++ mmdetection/mmdet/models/backbones/__init__.py
@@ -3,5 +3,7 @@ from .resnet import ResNet, make_res_layer
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 from .siamese_resnet import SiameseResNet
+from .siamese_resnext import SiameseResNeXt
+from .siamese_hrnet import SiameseHRNet
 
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'SiameseResNet']
+__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'SiameseResNet', 'SiameseResNeXt', 'SiameseHRNet']
