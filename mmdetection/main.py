e372de77273f1b4f574afef31da5fb950bc379f2
Leeeon233 <leeeon233@gmail.com>
2019-10-12 11:10:41 +0800

    seblock small

--- mmdetection/main.py
+++ mmdetection/main.py
@@ -24,9 +24,9 @@ class Detector:
                     scores.append(score)
                     rs.append(
                         {'name': image_name, 'category': defect_label, 'bbox': [x1, y1, x2, y2], 'score': score})
-            if len(scores) > 0 and max(scores) > 0.01:
-                if len(scores) == 1 and max(scores) < 0.05:
-                    continue
+            if len(scores) > 0 and max(scores) > 0.05:
+                # if len(scores) == 1 and max(scores) < 0.05:
+                #     continue
                 result += rs
         return result
 
