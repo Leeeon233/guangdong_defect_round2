d2df351e9daefc8de169750bdbe6f8c28d7abdf6
Leeeon233 <leeeon233@gmail.com>
2019-10-20 20:15:29 +0800

    merge test

--- mmdetection/myconfig/101/grid_rcnn_gn_head_x101_32x4d_fpn_2x.py
+++ mmdetection/myconfig/101/grid_rcnn_gn_head_x101_32x4d_fpn_2x.py
@@ -111,7 +111,7 @@ test_cfg = dict(
         nms_thr=0.7,
         min_bbox_size=0),
     rcnn=dict(
-        score_thr=0.001, nms=dict(type='soft_nms', iou_thr=0.5, method='gaussian'), max_per_img=100))
+        score_thr=0.05, nms=dict(type='soft_nms', iou_thr=0.5, method='gaussian'), max_per_img=100))
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = '/shared_disk/hannah/datasets/guangdong_round2/process/siamese_coco_round2/'#'/shared_disk/zhaoliang/datasets/guangdong_round2/siamese_coco_aug_large/'
